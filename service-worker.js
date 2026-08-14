35ff62b94e975194ec963ea755555a934e88635c
PUBULUS <148194671+PUBULUS@users.noreply.github.com>
2026-08-14 12:41:20 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const C="watch-roulette-v2.4",A=["./","./index.html","./manifest.json","./css/styles.css","./js/app.js","./icons/icon.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
